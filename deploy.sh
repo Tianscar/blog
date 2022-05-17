48f202e16ac20c63e4eb88d755e683f90ee459f3
tianscar <tianscar@protonmail.com>
2022-05-17 20:02:36 +0800

    Site updated: 2022-05-17 20:02:36

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/sh
 
-hexo clean && hexo deploy
 git add .
 git commit -m "Site updated: $(date "+%Y-%m-%d %H:%M:%S")"
-git pull
-git push -u origin main
+git pull origin main
+git push origin main
+
+hexo clean && hexo deploy
