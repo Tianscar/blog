131d0c6833d708d575ad608d86baeebf73bb427b
Karstian Lee <tianscar@protonmail.com>
2023-09-10 06:38:50 -0700

    Site updated: 2023-09-10 06:38:50

--- publish.sh
+++ publish.sh
@@ -5,4 +5,10 @@ git commit -m "Site updated: $(date "+%Y-%m-%d %H:%M:%S")"
 git pull origin main
 git push origin main
 
-npx hexo clean && npx hexo deploy
+npx hexo clean && npx hexo generate
+
+cd public
+git init
+git add .
+git commit -m "Site updated: $(date "+%Y-%m-%d %H:%M:%S")"
+git push -u git@github.com:Tianscar/blog.git origin gh-pages --force
